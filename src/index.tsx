5cb3a35c2be24b30d2e7637beb9c740282ddf0f6
dpasque <dan.speckhard.pasque@automattic.com>
2022-10-13 09:43:32 -0500

    Delete unneeded files, clean up structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,4 +15,6 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// TODO: might not be a bad idea to add this in the future.
+// reportWebVitals();
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,6 @@
+// This is hard-coded file required by the Create React App scripts.
+// When we inevitably "eject" and go to our own build scripts, we will move this.
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
