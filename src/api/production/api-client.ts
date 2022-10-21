6f35aa68ebf9786969192355498ac31dcfac70fe
dpasque <dan.speckhard.pasque@automattic.com>
2022-10-21 12:10:19 -0500

    Add placeholders for the production API client

---
+++ src/api/production/api-client.ts
@@ -0,0 +1,11 @@
+import { ApiClient } from '../types';
+
+/**
+ * Placeholder for the "real" API client for making real HTTP calls to the backend REST API.
+ * This will ultimately become a more complicated class that handles auth, etc.
+ */
+export const productionApiClient: ApiClient = {
+	loadReportingConfig: async () => {
+		throw new Error( 'Not implemented!' );
+	},
+};
