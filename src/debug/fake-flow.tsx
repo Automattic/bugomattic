c368d199d79c4a2b760012d4c002acbc2476969f
dpasque <dan.speckhard.pasque@automattic.com>
2023-01-17 15:23:18 -0600

    Add fake step to test component

--- src/debug/fake-flow.tsx
+++ src/debug/fake-flow.tsx
@@ -1,8 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { FeatureSelectorForm } from '../feature-selector-form/feature-selector-form';
 import { useMonitoring } from '../monitoring/monitoring-provider';
 import { NextSteps } from '../next-steps/next-steps';
+import { StepContainer } from '../reporting-flow-steps/step-container';
 import { TitleTypeForm } from '../title-type-form/title-type-form';
+
+function FakeStep() {
+	const [ isActive, setIsActive ] = useState( false );
+
+	const onContinue = () => setIsActive( false );
+	const onEdit = useCallback( () => setIsActive( true ), [] );
+
+	const activeContent = (
+		<div>
+			<p>This step is being edited currently.</p>
+			<button className="primaryButton" onClick={ onContinue }>
+				Continue
+			</button>
+		</div>
+	);
+
+	const inactiveContent = (
+		<div>
+			<p>This step is complete/inactive.</p>
+		</div>
+	);
+
+	const stepContent = isActive ? activeContent : inactiveContent;
+
+	return (
+		<StepContainer
+			stepNumber={ 1 }
+			title="Fake Test Step"
+			isComplete={ ! isActive }
+			showEditButton={ ! isActive }
+			onEdit={ onEdit }
+		>
+			{ stepContent }
+		</StepContainer>
+	);
+}
 
 export function FakeFlow() {
 	const monitoring = useMonitoring();
@@ -15,6 +52,9 @@ export function FakeFlow() {
 	);
 	return (
 		<div>
+			<div style={ { marginBottom: '32px' } }>
+				<FakeStep />
+			</div>
 			<div>
 				<TitleTypeForm />
 			</div>
