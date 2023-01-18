b6d9875bd8b3b60082f0be83b8f4da81071faa0e
dpasque <dan.speckhard.pasque@automattic.com>
2023-01-18 16:21:09 -0600

    Unit test App component

--- src/app/__tests__/app.test.tsx
+++ src/app/__tests__/app.test.tsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { screen, waitForElementToBeRemoved } from '@testing-library/react';
 import { App } from '../app';
-import { ApiClient } from '../../api/types';
+import { ApiClient, ReportingConfigApiResponse } from '../../api/types';
 import { createMockApiClient } from '../../test-utils/mock-api-client';
 import { renderWithProviders } from '../../test-utils/render-with-providers';
 
 describe( '[app]', () => {
-	let apiClient: ApiClient;
+	function setup( component: ReactElement, apiClient: ApiClient ) {
+		renderWithProviders( component, {
+			apiClient,
+		} );
+	}
 
-	beforeEach( () => {
-		apiClient = createMockApiClient();
-	} );
+	test( 'App shows loading indicator until reporting config is loaded, then the reporting flow', async () => {
+		const apiClient = createMockApiClient();
+		// This function will get set to the resolve() function in the API client call.
+		let resolveRequestPromise: ( config: ReportingConfigApiResponse ) => void = () => {
+			throw new Error( 'loadReportingConfig was not called in mocked API Client' );
+		};
+		const requestPromise = new Promise< ReportingConfigApiResponse >( ( resolve ) => {
+			resolveRequestPromise = resolve;
+		} );
+		apiClient.loadReportingConfig.mockReturnValue( requestPromise );
 
-	test( 'Loading indicator goes away once reporting config is loaded', async () => {
-		renderWithProviders( <App />, { apiClient } );
-		await waitForElementToBeRemoved( () => screen.queryByText( 'Reporting config is loading...' ) );
+		setup( <App />, apiClient );
+
+		// Loading indicator is present...
+		expect(
+			screen.getByRole( 'dialog', { name: 'Loading issue reporting configuration...' } )
+		).toBeInTheDocument();
+		// ...Reporting flow is not.
+		expect(
+			screen.queryByRole( 'heading', { name: 'Report a new issue' } )
+		).not.toBeInTheDocument();
+
+		resolveRequestPromise( {} );
+
+		// Then it flips!
+
+		await waitForElementToBeRemoved( () =>
+			screen.queryByRole( 'dialog', { name: 'Loading issue reporting configuration...' } )
+		);
+
+		expect(
+			await screen.findByRole( 'heading', { name: 'Report a new issue' } )
+		).toBeInTheDocument();
 	} );
 } );
