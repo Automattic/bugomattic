f72ed689a0d941f3c5ee649548efd215dbf6900e
dpasque <dan.speckhard.pasque@automattic.com>
2023-01-24 15:08:54 -0600

    Aria hide the confetti

--- src/next-steps/next-steps.tsx
+++ src/next-steps/next-steps.tsx
@@ -56,6 +56,7 @@ export function NextSteps() {
 		<section ref={ sectionRef } className={ styles.sectionWrapper }>
 			{ showConfetti && (
 				<Confetti
+					aria-hidden={ true }
 					data-testid="confetti"
 					height={ sectionHeight }
 					width={ sectionWidth }
