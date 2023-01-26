15ad33ef5449bac8054a7b0ba2f57af85c5cf6e6
Dan Speckhard Pasque <dan.speckhard.pasque@automattic.com>
2023-01-26 16:17:49 -0600

    Add more information and confetti to the next steps components (#27)
    * Add more info components
    
    * Add confetti!
    
    * Add some app bottom margin

--- src/next-steps/next-steps.tsx
+++ src/next-steps/next-steps.tsx
@@ -1,19 +1,84 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useAppSelector } from '../app/hooks';
 import { selectRelevantTaskIds } from '../combined-selectors/relevant-task-ids';
 import { Task } from './sub-components/task';
 import styles from './next-steps.module.css';
+import { MoreInfo } from './sub-components/more-info';
+import Confetti from 'react-confetti';
+import { selectIssueFeatureId } from '../issue-details/issue-details-slice';
+import { selectAllTasksAreComplete } from '../combined-selectors/all-tasks-are-complete';
 
 export function NextSteps() {
+	const sectionRef = useRef< HTMLElement >( null );
+	const [ sectionWidth, setSectionWidth ] = useState( 0 );
+	const [ sectionHeight, setSectionHeight ] = useState( 0 );
+	const [ showConfetti, setShowConfetti ] = useState( false );
+
+	const issueFeatureId = useAppSelector( selectIssueFeatureId );
 	const relevantTaskIds = useAppSelector( selectRelevantTaskIds );
+	const allTasksAreComplete = useAppSelector( selectAllTasksAreComplete );
 
-	const taskListDisplay = (
-		<ol aria-label="Steps to report issue" className={ styles.taskList }>
-			{ relevantTaskIds.map( ( taskId ) => (
-				<Task key={ taskId } taskId={ taskId } />
-			) ) }
-		</ol>
+	const updateSizes = () => {
+		if ( sectionRef.current ) {
+			// If available, we want to calculate the width and height of the parent element,
+			// which will the step container wrapping this section.
+			// That lets us make the whole step feel like it has confetti!
+			// If that's not available, let's fall back to the width and height of just this section.
+			setSectionWidth(
+				sectionRef.current.parentElement?.clientWidth ?? sectionRef.current.clientWidth
+			);
+			setSectionHeight(
+				sectionRef.current.parentElement?.clientHeight ?? sectionRef.current.clientHeight
+			);
+		}
+	};
+
+	useEffect( () => {
+		if ( allTasksAreComplete ) {
+			setShowConfetti( true );
+		} else {
+			setShowConfetti( false );
+		}
+	}, [ allTasksAreComplete ] );
+
+	// On mount, we want to calculate the section size for the Confetti
+	// We also want to recalculate whenever the relevant tasks change, or the selected feature
+	// changes, as both can affect the overall size of this section.
+	useEffect( () => {
+		updateSizes();
+	}, [ issueFeatureId, relevantTaskIds ] );
+
+	// Finally, we want to recalculate on every window resize!
+	useEffect( () => {
+		globalThis.addEventListener( 'resize', updateSizes );
+		return () => {
+			globalThis.removeEventListener( 'resize', updateSizes );
+		};
+	} );
+
+	const handleConfettiComplete = useCallback( () => {
+		setShowConfetti( false );
+	}, [] );
+
+	return (
+		<section ref={ sectionRef }>
+			{ showConfetti && (
+				<Confetti
+					aria-hidden={ true }
+					data-testid="confetti"
+					height={ sectionHeight }
+					width={ sectionWidth }
+					recycle={ false }
+					initialVelocityY={ { min: 0, max: 15 } }
+					onConfettiComplete={ handleConfettiComplete }
+				/>
+			) }
+			<ol aria-label="Steps to report issue" className={ styles.taskList }>
+				{ relevantTaskIds.map( ( taskId ) => (
+					<Task key={ taskId } taskId={ taskId } />
+				) ) }
+			</ol>
+			<MoreInfo />
+		</section>
 	);
-
-	return <section>{ taskListDisplay }</section>;
 }
---
+++ src/next-steps/sub-components/__tests__/more-info.test.tsx
@@ -0,0 +1,258 @@
+import React, { ReactElement } from 'react';
+import userEvent from '@testing-library/user-event';
+import { createMockApiClient } from '../../../test-utils/mock-api-client';
+import { NormalizedReportingConfig } from '../../../reporting-config/types';
+import { renderWithProviders } from '../../../test-utils/render-with-providers';
+import { MoreInfo } from '../more-info';
+import { screen } from '@testing-library/react';
+
+describe( '[MoreInfo]', () => {
+	function setup(
+		component: ReactElement,
+		featureId: string,
+		reportingConfig: NormalizedReportingConfig
+	) {
+		const apiClient = createMockApiClient();
+		const user = userEvent.setup();
+		const view = renderWithProviders( component, {
+			apiClient,
+			preloadedState: {
+				reportingConfig: {
+					normalized: reportingConfig,
+					// eslint-disable-next-line @typescript-eslint/no-explicit-any
+					indexed: {} as any,
+					status: 'loaded',
+					error: null,
+				},
+				issueDetails: {
+					issueType: 'bug',
+					featureId: featureId,
+					issueTitle: '',
+				},
+			},
+		} );
+
+		return {
+			user,
+			...view,
+		};
+	}
+
+	describe( 'Entity availability', () => {
+		function createStartingReportingConfig() {
+			const featureId = 'featureId';
+			// Use a mixture of description and link settings to help cover supporting both
+			const reportingConfig: NormalizedReportingConfig = {
+				products: {
+					productId: {
+						id: 'productId',
+						name: 'Product',
+						description: 'Product description',
+						featureIds: [],
+						featureGroupIds: [ 'featureGroupId' ],
+					},
+				},
+				featureGroups: {
+					featureGroupId: {
+						id: 'featureGroupId',
+						name: 'Feature Group',
+						learnMoreLinks: [
+							{
+								type: 'slack',
+								channel: 'channelName',
+							},
+						],
+						featureIds: [ featureId ],
+						productId: 'productId',
+					},
+				},
+				features: {
+					[ featureId ]: {
+						id: featureId,
+						name: 'Feature',
+						description: 'Feature description',
+						learnMoreLinks: [ { type: 'p2', subdomain: 'p2subdomain' } ],
+						parentType: 'featureGroup',
+						parentId: 'featureGroupId',
+					},
+				},
+				tasks: {},
+			};
+
+			return {
+				featureId,
+				reportingConfig,
+			};
+		}
+
+		it( 'If the feature has no description or links, it is excluded', async () => {
+			const { featureId, reportingConfig } = createStartingReportingConfig();
+			delete reportingConfig.features[ featureId ].description;
+			delete reportingConfig.features[ featureId ].learnMoreLinks;
+
+			setup( <MoreInfo />, featureId, reportingConfig );
+
+			// Other sections are still present
+			expect( screen.getByRole( 'heading', { name: 'Product', exact: true } ) ).toBeInTheDocument();
+			expect(
+				screen.getByRole( 'heading', { name: 'Feature Group', exact: true } )
+			).toBeInTheDocument();
+
+			// Feature info is not
+			expect(
+				screen.queryByRole( 'heading', { name: 'Feature', exact: true } )
+			).not.toBeInTheDocument();
+		} );
+
+		it( 'If the feature group has no description or links, it is excluded', async () => {
+			const { featureId, reportingConfig } = createStartingReportingConfig();
+			delete reportingConfig.featureGroups[ 'featureGroupId' ].description;
+			// Mix it up with an empty array of learn more links
+			reportingConfig.featureGroups[ 'featureGroupId' ].learnMoreLinks = [];
+
+			setup( <MoreInfo />, featureId, reportingConfig );
+
+			// Other sections are still present
+			expect( screen.getByRole( 'heading', { name: 'Product', exact: true } ) ).toBeInTheDocument();
+			expect( screen.getByRole( 'heading', { name: 'Feature', exact: true } ) ).toBeInTheDocument();
+
+			// Feature group info is not
+			expect(
+				screen.queryByRole( 'heading', { name: 'Feature Group', exact: true } )
+			).not.toBeInTheDocument();
+		} );
+
+		it( 'If the feature is directly under a product, no feature group info is included', async () => {
+			const { featureId, reportingConfig } = createStartingReportingConfig();
+			// We're cheesing this a bit for simplicity. We should really update both directions, but this is
+			// the only one that matters here, and keeps the test quite a bit more simple.
+			reportingConfig.features[ featureId ].parentType = 'product';
+			reportingConfig.features[ featureId ].parentId = 'productId';
+
+			setup( <MoreInfo />, featureId, reportingConfig );
+
+			// Other sections are still present
+			expect( screen.getByRole( 'heading', { name: 'Product', exact: true } ) ).toBeInTheDocument();
+			expect( screen.getByRole( 'heading', { name: 'Feature', exact: true } ) ).toBeInTheDocument();
+
+			// Feature group info is not
+			expect(
+				screen.queryByRole( 'heading', { name: 'Feature Group', exact: true } )
+			).not.toBeInTheDocument();
+		} );
+
+		it( 'If the product has no description or links, it is excluded', async () => {
+			const { featureId, reportingConfig } = createStartingReportingConfig();
+			delete reportingConfig.products[ 'productId' ].description;
+			delete reportingConfig.products[ 'productId' ].learnMoreLinks;
+
+			setup( <MoreInfo />, featureId, reportingConfig );
+
+			// Other sections are still present
+			expect(
+				screen.getByRole( 'heading', { name: 'Feature Group', exact: true } )
+			).toBeInTheDocument();
+			expect( screen.getByRole( 'heading', { name: 'Feature', exact: true } ) ).toBeInTheDocument();
+
+			// Feature group info is not
+			expect(
+				screen.queryByRole( 'heading', { name: 'Product', exact: true } )
+			).not.toBeInTheDocument();
+		} );
+
+		it( 'If no entities have any descriptions or links, the whole section does not display', async () => {
+			const { featureId, reportingConfig } = createStartingReportingConfig();
+			delete reportingConfig.products[ 'productId' ].description;
+			delete reportingConfig.products[ 'productId' ].learnMoreLinks;
+			delete reportingConfig.featureGroups[ 'featureGroupId' ].description;
+			delete reportingConfig.featureGroups[ 'featureGroupId' ].learnMoreLinks;
+			delete reportingConfig.features[ featureId ].description;
+			delete reportingConfig.features[ featureId ].learnMoreLinks;
+
+			setup( <MoreInfo />, featureId, reportingConfig );
+
+			expect(
+				screen.queryByRole( 'heading', { name: 'More Information', exact: true } )
+			).not.toBeInTheDocument();
+			expect(
+				screen.queryByRole( 'heading', { name: 'Product', exact: true } )
+			).not.toBeInTheDocument();
+			expect(
+				screen.queryByRole( 'heading', { name: 'Feature Group', exact: true } )
+			).not.toBeInTheDocument();
+			expect(
+				screen.queryByRole( 'heading', { name: 'Feature', exact: true } )
+			).not.toBeInTheDocument();
+		} );
+	} );
+
+	describe( 'Content display', () => {
+		const featureId = 'featureId';
+		const description = 'feature description unique text';
+		const reportingConfig: NormalizedReportingConfig = {
+			products: {
+				productId: {
+					id: 'productId',
+					name: 'Product',
+					featureIds: [ featureId ],
+					featureGroupIds: [],
+				},
+			},
+			featureGroups: {},
+			features: {
+				[ featureId ]: {
+					id: featureId,
+					name: 'Feature',
+					description: description,
+					learnMoreLinks: [
+						{ type: 'p2', subdomain: 'p2subdomain' },
+						{ type: 'p2', subdomain: 'p2subdomainwithtext', displayText: 'P2 display text' },
+						{ type: 'slack', channel: 'slack-channel' },
+						{
+							type: 'slack',
+							channel: 'slack-channel-with-text',
+							displayText: 'Slack display text',
+						},
+						{ type: 'general', href: 'https://automattic.com' },
+						{ type: 'general', href: 'https://google.com', displayText: 'General display text' },
+					],
+					parentType: 'product',
+					parentId: 'productId',
+				},
+			},
+			tasks: {},
+		};
+
+		test( 'By default, P2 links have a display text of "+subdomain"', async () => {
+			setup( <MoreInfo />, featureId, reportingConfig );
+
+			expect( screen.getByRole( 'link', { name: '+p2subdomain' } ) ).toBeInTheDocument();
+		} );
+
+		test( 'By default, Slack links have a display text of "#channel"', async () => {
+			setup( <MoreInfo />, featureId, reportingConfig );
+
+			expect( screen.getByRole( 'link', { name: '#slack-channel' } ) ).toBeInTheDocument();
+		} );
+
+		test( 'By default, General links have a display text of "#channel"', async () => {
+			setup( <MoreInfo />, featureId, reportingConfig );
+
+			expect( screen.getByRole( 'link', { name: 'https://automattic.com' } ) ).toBeInTheDocument();
+		} );
+
+		test( 'All links support adding display text', async () => {
+			setup( <MoreInfo />, featureId, reportingConfig );
+
+			expect( screen.getByRole( 'link', { name: 'P2 display text' } ) ).toBeInTheDocument();
+			expect( screen.getByRole( 'link', { name: 'Slack display text' } ) ).toBeInTheDocument();
+			expect( screen.getByRole( 'link', { name: 'General display text' } ) ).toBeInTheDocument();
+		} );
+
+		test( 'If the entity has a description, it is displayed', async () => {
+			setup( <MoreInfo />, featureId, reportingConfig );
+
+			expect( screen.getByText( description ) ).toBeInTheDocument();
+		} );
+	} );
+} );
