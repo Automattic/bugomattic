8c5a10a23e31d9110b317c6d7968df85acc038fb
dpasque <dan.speckhard.pasque@automattic.com>
2023-03-30 09:27:05 -0500

    Fix static data thunk typing!

--- src/static-data/load-static-data.ts
+++ src/static-data/load-static-data.ts
@@ -2,7 +2,7 @@ import { AppThunk } from '../app/store';
 import { loadAvailableRepoFilters } from './available-repo-filters/available-repo-filters-slice';
 import { loadReportingConfig } from './reporting-config/reporting-config-slice';
 
-export const loadStaticData: () => AppThunk = () => async ( dispatch ) => {
+export const loadStaticData: () => AppThunk< Promise< void > > = () => async ( dispatch ) => {
 	await Promise.all( [
 		dispatch( loadAvailableRepoFilters() ),
 		dispatch( loadReportingConfig() ),
