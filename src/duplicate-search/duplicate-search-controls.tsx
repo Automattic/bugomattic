b84619b1774833d7d4658cf49906bd59b0b5b2a9
dpasque <dan.speckhard.pasque@automattic.com>
2023-04-21 13:07:48 -0500

    Add search bar

--- src/duplicate-search/duplicate-search-controls.tsx
+++ src/duplicate-search/duplicate-search-controls.tsx
@@ -1,35 +1,28 @@
 import React from 'react';
-import { useAppDispatch, useAppSelector } from '../app/hooks';
-import { selectAvailableRepoFilters } from '../static-data/available-repo-filters/available-repo-filters-slice';
-import {
-	setActiveRepoFilters,
-	setSearchTerm,
-	setSort,
-	setStatusFilter,
-	setSearchParam,
-} from './duplicate-search-slice';
 import { RepoFilter, StatusFilter } from './sub-components';
+import { useAppDispatch } from '../app/hooks';
+import { setSearchTerm, setSearchParam } from './duplicate-search-slice';
+import { DebouncedSearch } from '../common/components';
 import styles from './duplicate-search-controls.module.css';
 
 // TODO: This is a placeholder component for the duplicate search controls. Modify and tweak however needed! :)
 export function DuplicateSearchControls() {
 	const dispatch = useAppDispatch();
-	const repoFilters = useAppSelector( selectAvailableRepoFilters );
 
-	const handleFauxSearch = () => {
-		dispatch( setSearchTerm( 'Test search term' ) );
-		dispatch( setStatusFilter( 'open' ) );
-		dispatch( setActiveRepoFilters( [] ) );
-
-		// What the dispatch in most components will actually look like
-		dispatch( setSearchParam( setSort( 'relevance' ) ) );
+	const handleSearchTerm = ( searchTerm: string ) => {
+		dispatch( setSearchParam( setSearchTerm( searchTerm ) ) );
 	};
 
 	return (
 		<section>
-			<p>This is just a filler component</p>
-			<button onClick={ handleFauxSearch }>Faux search</button>
-			<p>Available repo filters: { repoFilters.join( ', ' ) }</p>
+			<div>
+				<DebouncedSearch
+					placeholder="Search"
+					callback={ handleSearchTerm }
+					inputAriaLabel="Search for duplicate issues"
+					debounceMs={ 500 }
+				/>
+			</div>
 			<div className={ styles.filterSortBar }>
 				<div className={ styles.filterControls }>
 					<StatusFilter />
