d5fca935728d8d566d1e88926993a43128823db8
dpasque <dan.speckhard.pasque@automattic.com>
2023-04-21 13:20:49 -0500

    Update styles

--- src/duplicate-search/duplicate-search-controls.tsx
+++ src/duplicate-search/duplicate-search-controls.tsx
@@ -15,7 +15,7 @@ export function DuplicateSearchControls() {
 
 	return (
 		<section>
-			<div>
+			<div className={ styles.searchWrapper }>
 				<DebouncedSearch
 					placeholder="Search"
 					callback={ handleSearchTerm }
