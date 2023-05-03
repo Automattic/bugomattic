7e257a6d364e8af25f570e1dbc5a5815d7a39e1a
dpasque <dan.speckhard.pasque@automattic.com>
2023-05-03 16:47:35 -0500

    Add real styles to the duplicate searching page

--- src/duplicate-searching-page/duplicate-searching-page.tsx
+++ src/duplicate-searching-page/duplicate-searching-page.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 import { DuplicateResults } from '../duplicate-results/duplicate-results';
 import { DuplicateSearchControls } from '../duplicate-search/duplicate-search-controls';
 import { usePageNavigation } from '../active-page/page-navigation-provider';
+import styles from './duplicate-searching-page.module.css';
 
 export function DuplicateSearchingPage() {
 	const { pageHeadingRef } = usePageNavigation();
 	return (
-		<section
-			// TODO: update with a real CSS module for this overall page.
-			style={ {
-				maxWidth: '920px',
-				margin: '0 auto',
-			} }
-		>
+		<section className={ styles.wrapper }>
 			<h2 ref={ pageHeadingRef } className="screenReaderOnly" tabIndex={ -1 }>
 				Search for duplicate issues
 			</h2>
