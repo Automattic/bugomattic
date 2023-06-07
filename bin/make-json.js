75b1fc409c2a48d28f04041d0240fad0ad444d3e
karenroldan <mail@karenroldan.net>
2023-06-07 15:05:16 +0200

    Add features and relevant information

--- bin/make-json.js
+++ bin/make-json.js
@@ -12,35 +12,43 @@ const parsedCsv = papa.parse( inputCsv, { header: true } );
 const jsonString = fs.readFileSync( outputJsonPath, { encoding: 'utf8' } );
 const outputJson = JSON.parse( jsonString );
 
-const productName = 'WordPress.com';
-if ( ! outputJson[ productName ] ) {
-	outputJson[ productName ] = {};
-}
-
-const product = outputJson[ productName ];
-
-if ( ! product.featureGroups ) {
-	product.featureGroups = {};
-}
-
 let lastFeatureGroup;
+
 for ( const row of parsedCsv.data ) {
-	const { name, isGroup, description, keywords } = row;
+	const { name, isGroup, description, keywords, labels, projectSlugs, productName } = row;
+
+	if ( ! outputJson[ productName ] ) {
+		outputJson[ productName ] = {};
+	}
+
+	const product = outputJson[ productName ];
+
+	if ( ! product.featureGroups ) {
+		product.featureGroups = {};
+	}
+
+	if ( ! product.features ) {
+		product.features = {};
+	}
+
 	if ( isGroup.toLowerCase() === 'yes' ) {
 		const featureGroup = {
 			description,
 			features: {},
 		};
+
 		if ( hasLearnMoreData( row ) ) {
 			featureGroup.learnMoreLinks = makeLearnMoreLinks( row );
 		}
 
 		product.featureGroups[ name ] = featureGroup;
-		lastFeatureGroup = featureGroup;
+		lastFeatureGroup = name; // This should be name, not featureGroup
 	} else {
 		const feature = {
 			description,
 		};
+		let additionalLabels = [];
+		let projectSlugsList = [];
 
 		if ( hasLearnMoreData( row ) ) {
 			feature.learnMoreLinks = makeLearnMoreLinks( row );
@@ -50,16 +58,26 @@ for ( const row of parsedCsv.data ) {
 			feature.keywords = keywords.split( ',' ).map( ( keyword ) => keyword.trim() );
 		}
 
+		if ( labels ) {
+			additionalLabels = labels.split( ',' ).map( ( label ) => label.trim() );
+		}
+
+		if ( projectSlugs ) {
+			projectSlugsList = projectSlugs.split( ',' ).map( ( slug ) => slug.trim() );
+		}
+
 		const labelName = `[Feature] ${ name.trim() }`;
+		const { repository, bugTemplate, featureRequestTemplate } = getRepositoryAndTemplateData( row );
+
 		feature.tasks = {
 			bug: [
 				{
 					link: {
 						type: 'github',
-						repository: 'Automattic/wp-calypso',
-						template: 'bug_report.yml',
-						labels: [ labelName ],
-						projectSlugs: [ 'Automattic/343' ],
+						repository: repository,
+						template: bugTemplate,
+						labels: [ labelName, ...additionalLabels ],
+						projectSlugs: [ 'Automattic/343', ...projectSlugsList ],
 					},
 				},
 			],
@@ -67,10 +85,10 @@ for ( const row of parsedCsv.data ) {
 				{
 					link: {
 						type: 'github',
-						repository: 'Automattic/wp-calypso',
-						template: 'feature_request.yml',
-						labels: [ labelName ],
-						projectSlugs: [ 'Automattic/343' ],
+						repository: repository,
+						template: featureRequestTemplate,
+						labels: [ labelName, ...additionalLabels ],
+						projectSlugs: [ 'Automattic/343', ...projectSlugsList ],
 					},
 				},
 			],
@@ -78,16 +96,23 @@ for ( const row of parsedCsv.data ) {
 				{
 					link: {
 						type: 'github',
-						repository: 'Automattic/wp-calypso',
-						template: 'bug_report.yml',
-						labels: [ '[Pri] BLOCKER', labelName ],
-						projectSlugs: [ 'Automattic/343' ],
+						repository: repository,
+						template: bugTemplate,
+						labels: [ '[Pri] BLOCKER', labelName, ...additionalLabels ],
+						projectSlugs: [ 'Automattic/343', ...projectSlugsList ],
 					},
 				},
 			],
 		};
 
-		lastFeatureGroup.features[ name ] = feature;
+		// Check if there's an active Feature Group to add the Feature to
+		if ( lastFeatureGroup && product.featureGroups[ lastFeatureGroup ] ) {
+			// If there's an active Feature Group, add the Feature to it
+			product.featureGroups[ lastFeatureGroup ].features[ name ] = feature;
+		} else {
+			// If there's no active Feature Group, add the Feature to the Product
+			product.features[ name ] = feature;
+		}
 	}
 }
 
@@ -133,4 +158,39 @@ function makeLearnMoreLinks( dataRow ) {
 	}
 
 	return links;
+}
+
+function setRepositoryDefaults( productName, defaultValue ) {
+	const defaultValues = {
+		'WordPress.com': 'Automattic/wp-calypso',
+		Jetpack: 'Automattic/jetpack',
+	};
+	return defaultValues[ productName ] || defaultValue;
+}
+
+function setTemplateDefaults( productName, defaultValue ) {
+	const defaultValues = {
+		'WordPress.com': 'bug_report.yml',
+		Jetpack: 'bug-report.yml',
+	};
+	return defaultValues[ productName ] || defaultValue;
+}
+
+function getRepositoryAndTemplateData( dataRow ) {
+	const { repository, productName, template } = dataRow;
+	let featureRequestTemplateName = 'feature_request.yml';
+
+	let repositoryLink =
+		repository && repository.trim() !== '' ? repository : setRepositoryDefaults( productName, '' );
+
+	let bugTemplateName =
+		template && template.trim() !== ''
+			? template
+			: setTemplateDefaults( repositoryLink, 'bug_report.yml' );
+
+	return {
+		repository: repositoryLink,
+		bugTemplate: bugTemplateName,
+		featureRequestTemplate: featureRequestTemplateName,
+	};
 }
